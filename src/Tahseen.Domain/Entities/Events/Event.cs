3630b5fc717184ad3b616ae583675b940fa394c6
Sardorbek9343 <tolibjonovsardorbek12@gmail.com>
2023-10-09 20:51:12 +0500

    Done

--- src/Tahseen.Domain/Entities/Events/Event.cs
+++ src/Tahseen.Domain/Entities/Events/Event.cs
@@ -1,7 +1,15 @@
-﻿namespace Tahseen.Domain.Entities.Events
+﻿using Tahseen.Domain.Enums.Events;
+
+namespace Tahseen.Domain.Entities.Events
 {
     public class Event
     {
         public string Title { get; set; }
+        public string Description { get; set; } 
+        public string Location { get; set; }    
+        public long Participants { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public EventStatus Status { get; set; }
     }
 }
