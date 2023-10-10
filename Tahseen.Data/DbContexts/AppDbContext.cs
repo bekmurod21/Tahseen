1ac98987550675d1459124e2d7f073a240bb1731
AzizbekFullStackDev <144344579+AzizbekFullStackDev@users.noreply.github.com>
2023-10-10 11:27:13 +0500

    Generic Repository is added

---
+++ Tahseen.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tahseen.Domain.Entities;
+using Tahseen.Domain.Entities.Books;
+
+namespace Tahseen.Data.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=ProductOrderDb;Username=postgres;Password=0808080808");
+    }
+}
---
+++ Tahseen.Data/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tahseen.Data.DbContexts;
+using Tahseen.Data.IRepositories;
+using Tahseen.Domain.Commons;
+
+namespace Tahseen.Data.Repositories
+{
+    public class Repository<TEntity> : IRepostory<TEntity> where TEntity : Auditable
+    {
+        AppDbContext dbContext;
+        DbSet<TEntity> dbSet;
+        public Repository()
+        {
+            dbContext = new AppDbContext(); 
+            this.dbSet = dbContext.Set<TEntity>();  
+        }
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            await dbSet.AddAsync(entity);
+            dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(long Id)
+        {
+            var result = await dbSet.FirstOrDefaultAsync(e => e.Id == Id);
+            var entity = dbSet.Remove(result);
+            return true;
+        }
+
+        public IQueryable<TEntity> SelectAll()
+            => this.dbSet;
+        public async Task<TEntity> SelectByIdAsync(long Id)
+        {
+            var result = await this.dbSet.FirstOrDefaultAsync(e => e.Id == Id);
+            return result;
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            var result = (dbContext.Update(entity)).Entity;
+            await dbContext.SaveChangesAsync();
+            return result;
+        }
+
+    }
+}
