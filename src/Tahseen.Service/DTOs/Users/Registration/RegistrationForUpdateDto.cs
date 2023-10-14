c9391ada1617abc4d791d6aac0be5ee8dfd29fdc
AzizbekFullStackDev <144344579+AzizbekFullStackDev@users.noreply.github.com>
2023-10-14 11:26:33 +0500

    Committing changes before cherry-pick

---
+++ src/Tahseen.Service/DTOs/Users/Registration/RegistrationForUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tahseen.Service.DTOs.Users.Registration
+{
+    internal class RegistrationForUpdateDto
+    {
+    }
+}
