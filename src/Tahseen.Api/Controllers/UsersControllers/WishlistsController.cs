86f3c1c7ea92ed7d7cefa237277e8152f10b7f20
Ilhomjon-Azimov <ilhomjonazimov777@gmail.com>
2023-10-17 00:33:51 +0500

    Created Wishlists Api

---
+++ src/Tahseen.Api/Controllers/UsersControllers/WishlistsController.cs
@@ -0,0 +1,67 @@
+﻿using Tahseen.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Tahseen.Service.DTOs.Users.Wishlists;
+using Tahseen.Service.Interfaces.IUsersService;
+
+namespace Tahseen.Api.Controllers.UsersControllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WishlistsController : ControllerBase
+    {
+      
+        private readonly IWishlistService _wishlistService;
+        public WishlistsController(IWishlistService wishlistService)
+        {
+            this._wishlistService = wishlistService;
+        }
+
+        [HttpGet]
+        public IQueryable<IActionResult> GetAll()
+            => (IQueryable<IActionResult>)Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Successful",
+                Data = this._wishlistService.RetrieveAll()
+            });
+
+
+        [HttpGet("{id}")]
+        public async ValueTask<IActionResult> Get(long id)
+            => Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Successful",
+                Data = await this._wishlistService.RemoveAsync(id)
+            });
+
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(WishlistForCreationDto dto)
+            => Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Successful",
+                Data = await this._wishlistService.AddAsync(dto)
+            });
+
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(long id)
+            => Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Successful",
+                Data = await this._wishlistService.RemoveAsync(id)
+            });
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(WishlistForUpdateDto dto)
+            => Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Successful",
+                Data = await this._wishlistService.ModifyAsync(dto)
+            });
+    }
+}
