df26342bdc52ed37fa0a0ead872580b797035797
AzizbekFullStackDev <144344579+AzizbekFullStackDev@users.noreply.github.com>
2023-11-02 10:10:21 +0500

    Bug fix

--- src/Tahseen.Domain/Enums/Roles.cs
+++ src/Tahseen.Domain/Enums/Roles.cs
@@ -3,9 +3,9 @@ namespace Tahseen.Domain.Enums;
 public enum Roles
 {
     User = 0,
-    Pupil = 0,
-    Student = 0,
-    Librarian = 0,
-    Admin = 0,
-    SuperAdmin = 0
+    Pupil = 1,
+    Student = 2,
+    Librarian = 3,
+    Admin = 4,
+    SuperAdmin = 5
 }
